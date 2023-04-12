7c99c87addce0d5ff8cf71d08a05d9e40a645787
ADMIN <belaybirhanu407@gmail.com>
2023-04-12 17:37:05 +0300

    I have changed the frequency display to table format

--- text-analyzer/word_frequency.py
+++ text-analyzer/word_frequency.py
@@ -1,13 +1,22 @@
-from clean_text import clean_text
-words = clean_text()
 
+from clean_text import clean_text  # import clean_text
+top = 0
 # Returns the frequency of words by decreasing order.
 def word_frequency():
+    print("Enter 1 for all word frequencys ranks or top of you want")
+    try: 
+        top = int(input("Top of: "))
+    except (Exception):
+        print("Invalid choice")
+        print("Enter 1 for all word frequencys ranks or top of you want")
+
     """ dictionary = {'word':'amount'}
         'word': stands for all specific words in the list.
         'amount': total amount of the word, for all unique words in the list.
     """
     dictionary = {} 
+    
+    words = clean_text() #  asign the clean text to the words as a list.
 
     # take one word per iteration
     for word in words:
@@ -22,7 +31,15 @@ def word_frequency():
     # sort the words in decreasing order by their amout.
     sortedDictionary = dict(sorted(dictionary.items(), key=lambda w: w[1], reverse=True))
 
-    for word, key in sortedDictionary.items():
-        print(word,key)
+    count = 0
+    print('{:<20} {:<20}'.format("WORDS", "AMOUNT")) # title for displaying the dictionary
+    for word, amount in sortedDictionary.items():
+        if top == 1:   # if user choosed 1, always this case will be excuted.
+            print('{:<20} {:<20}'.format(word, amount))
+        else:
+            if count > top:  # if user choosed other than 1 words upto his choice will be displayed.
+                break
+            print('{:<20} {:<20}'.format(word, amount))
+            count += 1
 
- 
+    
