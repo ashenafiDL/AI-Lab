3b714ff4cb5f15ef36097b69ce1a35dffdb41220
ADMIN <belaybirhanu407@gmail.com>
2023-04-18 21:02:29 +0300

    some code refactore on GUI of the game

--- RSP-Belay/rock-scissors-paper.py
+++ RSP-Belay/rock-scissors-paper.py
@@ -38,54 +38,56 @@ def play_game(user_action):
     return "You chose: " + user_action + "\nComputer chose: " + computer_action + "\n" + result
 
 # define function for button click
-def button_click():
-    # get user input
-    user_input = entry.get().strip().lower()
-    if user_input == "":
-        output.delete('1.0', tk.END)
-        output.insert(tk.END, "Invalid input! \nPlease enter 'rock', 'paper', or 'scissors'." + "\n")
-        return
-    # clear text entry box
-    entry.delete(0, tk.END)
+def button_click(user_input):
+    global machine_score, your_score
     # update game output
     output.delete('1.0', tk.END)
     try:
         output.insert(tk.END, play_game(user_input) + "\n")
     except ValueError:
-        output.insert(tk.END, "Invalid input! \nPlease enter 'rock', 'paper', or 'scissors'." + "\n")
+        output.insert(tk.END, "Invalid input! \nPlease press 'rock', 'paper', or 'scissors'." + "\n")
     # update score
     score.delete('1.0', tk.END)
-    score.insert(tk.END, "Machine: " + str(machine_score) + "     You: " + str(your_score))
+    score.insert(tk.END, "Machine: " + str(machine_score) + "          You: " + str(your_score))
 
 # set up main window
 root = tk.Tk()
-root.title("Paper-Scissors-Rock Game")
+root.title("ROCK-PAPER-SCISSORS GAME")
 
-# create label and entry for user input
-label = tk.Label(root, text="Choose 'rock', 'paper', or 'scissors':")
+# define colors
+bg_color = "#ADD8E6"
+button_color = "#FFFFFF"
+text_color = "#000000"
+
+# set window background color
+root.config(bg=bg_color)
+
+# create label for user input
+label = tk.Label(root, text="   ROCK-PAPER-SCISSORS GAME   ", bg=bg_color, fg=text_color, font=('Arial', 18))
 label.pack(pady=10)
-entry = tk.Entry(root, width=30, font=('Arial', 14))
-entry.pack()
 
-# create button for submitting user input
-button = tk.Button(root, text="Submit", command=button_click, font=('Arial', 14))
-button.pack(pady=10)
+# create buttons for rock, paper, and scissors
+rock_button = tk.Button(root, text="Rock", bg="teal", fg=text_color, font=('Arial', 14), command=lambda: button_click("rock"))
+rock_button.pack(side="bottom", padx=10, pady=10, expand=True, fill='both')
 
-# create label for game output
-output_label = tk.Label(root, text="Game output:")
-output_label.pack(pady=10)
+paper_button = tk.Button(root, text="Paper", bg="teal", fg=text_color, font=('Arial', 14), command=lambda: button_click("paper"))
+paper_button.pack(side="bottom", padx=10, pady=10, expand=True, fill='both')
+
+scissors_button = tk.Button(root, text="Scissors", bg="teal", fg=text_color, font=('Arial', 14), command=lambda: button_click("scissors"))
+scissors_button.pack(side="bottom", padx=10, pady=10, expand=True, fill='both')
+ 
+# create text box for score
+score = tk.Text(root, height=1, width=30, font=('Arial', 14))
+score.pack()
+score.insert(tk.END, "Machine: 0            You: 0")
+
+# create label for game output 
+output_label = tk.Label(root, text="Game output:", bg=bg_color, fg=text_color, font=('Arial', 18))
+output_label.pack(pady=20)
 
 # create text box for game output
 output = tk.Text(root, height=5, width=30, font=('Arial', 14))
 output.pack()
-
-# create label for score
-score_label = tk.Label(root, text="Score:")
-score_label.pack(pady=10)
-
-# create text box for score
-score = tk.Text(root, height=1, width=30, font=('Arial', 14))
-score.pack()
 
 # start main loop
 root.mainloop()
